9c34c25ce5318223ad97a93619775f3717a5ad2b
unknown <tilakbhati91@gmail.com>
2022-07-04 01:04:13 -0700

    tableprint

--- tableofanynumberenteredbyuser.c
+++ tableofanynumberenteredbyuser.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int n,i,m;
-printf ("enter any number:\n");
-scanf("%d",&n);
-printf ("the table is:\n");
-for(i=1;i<=10;i++)
-{
-m=n*i;
-printf("%d\n",m);
-}
-return 0;
+    int n,i,m;
+    printf("enter a number\n");
+    scanf("%d",&n);
+    printf("the table is:\n");
+    for (i = 1; i <= 10; i++)
+    {
+        m=n*i;
+        printf("%d\n",m);
+    }
+    
+    
+    return 0;
 }
